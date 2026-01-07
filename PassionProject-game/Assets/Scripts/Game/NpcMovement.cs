562ab050337b22237738888480523bc62fdf6296
Liz Geerts <liz.geerts@student.howest.be>
2026-01-07 21:17:28 +0100

    refinement of NPC movement, still not good, will work on it tommorrow

--- PassionProject-game/Assets/Scripts/Game/NpcMovement.cs
+++ PassionProject-game/Assets/Scripts/Game/NpcMovement.cs
@@ -2,92 +2,142 @@ using System;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
+
 public class NPCcontroller : MonoBehaviour
 {
 
+
     public GameObject ball;
+    private Vector3 target;
+
+
+    private Ballcontroller ballController;
+
 
     public float moveSpeed = 3f;
     public float stopDistance = 1.2f;
 
+
     private Animator animator;
     private bool ballInRange = false;
 
+
     public enum CourtSide { Left, Right }
     public CourtSide side;
+
 
     public float swingCooldown = 1f;
     private float lastSwingTime = 0f;
 
 
+    public Transform teammate;
+    public float teamFollowWeight = 0.3f;
+
+
+    private bool canMoveZ;
+    private bool isClosest;
+    private Vector3 moveDirection;
+    public float minX; //get them to stay in their lane
+    public float maxX;
+
+
+
     void Start()
     {
         animator = GetComponent<Animator>();
+        ballController = ball.GetComponent<Ballcontroller>();
     }
+
+
+    void MoveZ()
+    {
+        //is the ball on my side of the court
+        bool ballOnMySide = (side == CourtSide.Left && ballController.leftSide) ||
+        (side == CourtSide.Right && ballController.rightSide);
+
+
+        //is the ball behind me
+        bool ballBehindMe =
+        (side == CourtSide.Left && target.z < transform.position.z) ||
+        (side == CourtSide.Right && target.z > transform.position.z);
+
+
+        canMoveZ = ballOnMySide && !ballBehindMe;
+    }
+
+
+    void ClosestOne()
+    {
+        float distance = Vector3.Distance(transform.position, target); //distance to ball
+        float teammateDist = Vector3.Distance(teammate.position, target); //teammates distance
+
+        isClosest = distance <= teammateDist; //is this npc closer
+    }
+
+
+    void ChooseMovement()
+    {
+        moveDirection = Vector3.zero;
+        target.y = transform.position.y; //keep npc from flying
+        target.x = Mathf.Clamp(target.x, minX, maxX);
+
+
+        if (canMoveZ && isClosest) //then you can fully move
+        {
+            moveDirection = target - transform.position;
+        }
+        else //otherwise only sideways
+        {
+            float xDiff = target.x - transform.position.x;
+            if (Math.Abs(xDiff) > 0.2f)
+            {
+                moveDirection = new Vector3(xDiff, 0, 0);
+            }
+        }
+    }
+
 
     void MoveTowardBall()
     {
 
-        Vector3 target = ball.transform.position;
-        target.y = transform.position.y;
-
-        float distance = Vector3.Distance(transform.position, target);
-
-        if (side == CourtSide.Left)
-        {
-            if (target.z > transform.position.z)
-            {
-                target.z = transform.position.z;
-            }
-        }
-        else
-        {
-            if (target.z < transform.position.z)
-            {
-                target.z = transform.position.z;
-            }
-        }
-
-        // float leftBound = -1.5f; 
-        // float rightBound = 1.5f;
-        // target.x = Mathf.Clamp(target.x, leftBound, rightBound);
-
-
-        if (distance > stopDistance)
-        {
-            bool ballOnMySide = (side == CourtSide.Left && ball.GetComponent<Ballcontroller>().leftSide) ||
-                    (side == CourtSide.Right && ball.GetComponent<Ballcontroller>().rightSide);
-
-            if (!ballOnMySide)
-            {
-                target.z = transform.position.z; 
-            }
-
-            Vector3 dir = (target - transform.position).normalized;
-            transform.position += dir * moveSpeed * Time.deltaTime;
-
-            animator.SetFloat("Direction", 1);
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 8f);
-        }
-        else
+        if (moveDirection.magnitude < 0.05f)
         {
             animator.SetFloat("Direction", 0);
+            return;
         }
+
+
+        Vector3 dir = moveDirection.normalized;
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
+
+        transform.rotation = Quaternion.Slerp(
+        transform.rotation,
+        Quaternion.LookRotation(dir),
+        Time.deltaTime * 8f
+        );
+
+
+        animator.SetFloat("Direction", 1);
     }
+
 
     void TrySwing()
     {
 
-        if (Time.time - lastSwingTime < swingCooldown) return; 
+
+        if (Time.time - lastSwingTime < swingCooldown) return;
+
 
         if (ballInRange)
         {
-           animator.SetTrigger("Forehand");
-           lastSwingTime = Time.time;
+            animator.SetTrigger("Forehand");
+            lastSwingTime = Time.time;
         }
 
+
     }
+
 
     void OnTriggerEnter(Collider other)
     {
@@ -98,6 +148,7 @@ public class NPCcontroller : MonoBehaviour
     }
 
 
+
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Ball"))
@@ -106,11 +157,18 @@ public class NPCcontroller : MonoBehaviour
         }
     }
 
+
     // Update is called once per frame
     void Update()
     {
+        target = ball.transform.position;
 
+
+        MoveZ();
+        ClosestOne();
+        ChooseMovement();
         MoveTowardBall();
+
 
         if (ballInRange)
         {
