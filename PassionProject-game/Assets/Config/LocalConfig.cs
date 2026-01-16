298322f0dfbe89b2c6c014883827ce1c713393ff
Liz Geerts <liz.geerts@student.howest.be>
2026-01-16 12:11:38 +0100

    had to commit

---
+++ PassionProject-game/Assets/Config/LocalConfig.cs
@@ -0,0 +1,15 @@
+namespace Config
+{
+  public static class LocalConfig
+  {
+    public const string ESP32_IP = "10.124.62.98";
+    public const string WIFI_SSID_DORM = "";
+    public const string WIFI_PASSWORD_DORM = "";
+
+    public const string WIFI_SSID_SCHOOL = "";
+    public const string WIFI_PASSWORD_SCHOOL = "";
+
+    public const string WIFI_SSID_HOME = "phoneLiz";
+    public const string WIFI_PASSWORD_HOME = "03056366855804472133";
+  }
+}
