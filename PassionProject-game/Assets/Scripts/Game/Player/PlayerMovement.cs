efd21c72cc54381c9bde30600588fa5d352db70d
Liz Geerts <liz.geerts@student.howest.be>
2026-01-18 16:05:39 +0100

    move player with joystick, fixes #14

--- PassionProject-game/Assets/Scripts/Game/Player/PlayerMovement.cs
+++ PassionProject-game/Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public CharacterController controller;
     public EspUdp espData;
     public Animator playerAnimation;
     //public EspConnect espData; => if using via cable not wifi
     public float moveSpeed = 3f;
-    private Vector3 velocity;
+
+    private bool lastRunLeft;
 
     void Start()
     {
@@ -15,6 +17,39 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        Vector3 move = Vector3.zero;
 
+        int dir = espData.joystickDir;
+        bool isMoving = false;
+
+        switch (dir)
+        {
+            case 1: move = transform.forward; isMoving = true; break;
+            case 2: move = -transform.forward; isMoving = true; break;
+            case 3:
+                {
+                    move = transform.right;
+                    lastRunLeft = false;
+                    isMoving = true;
+                    break;
+                }
+            case 4:
+                {
+                    move = -transform.right;
+                    lastRunLeft = true;
+                    isMoving = true;
+                    break;
+                }
+        }
+
+        float animationDir = 0f;
+        if (isMoving)
+        {
+            animationDir = lastRunLeft ? 1 : -1;
+        }
+
+        playerAnimation.SetFloat("Direction", animationDir);
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
     }
 }
