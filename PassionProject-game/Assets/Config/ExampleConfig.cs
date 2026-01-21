e257d0a509b375ae2297f3dd8173f81eca3ef812
Liz Geerts <liz.geerts@student.howest.be>
2026-01-21 08:40:34 +0100

    code cleanup

--- PassionProject-game/Assets/Config/ExampleConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace Config
-{
-  public static class ExampleConfig
-  {
-    public const string ESP32_IP = "ip";
-    public const string WIFI_SSID = "wifi-name";
-    public const string WIFI_PASSWORD = "wifi-password";
-  }
-}
